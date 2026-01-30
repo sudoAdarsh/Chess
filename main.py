ab9335e589e4007b7e1006fefaf96f77cdd8f041
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2026-01-30 12:11:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+# -------------------- CONSTANTS --------------------
+LIGHT = "#d6ebd5"
+DARK = "#528234"
+SELECTED = "#9fd3e6"
+LEGAL = "#b9ca4a"
+LAST = "#f5f682"
+FONT = ("DejaVu Sans", 60)
+
+
+# -------------------- BOARD SETUP --------------------
